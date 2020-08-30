0e1db312d5d70e98e9b33ee2bd5a1d0690fbba23
Arun Augustine <email.arunaugustine@gmail.com>
2020-08-30 18:04:56 +0530

    anonymous function definition (lambdas) now working

--- src/rome.rs
+++ src/rome.rs
@@ -52,8 +52,9 @@ pub enum RomeError {
  Let's go with Model for now.
 */
 #[derive(Clone)]
-pub struct Model {
+pub struct Model<'a> {
     store: HashMap<String, Oexp>,
+    inner: Option<&'a Model<'a>>, // conventionally we use outer environment
 }
 
 pub fn tokenise(input: String) -> Vec<String> {
@@ -108,7 +109,7 @@ fn parse_atom(token: &str) -> Oexp {
     }
 }
 
-pub fn new_core_model() -> Model {
+pub fn new_core_model<'a>() -> Model<'a> {
     let mut store: HashMap<String, Oexp> = HashMap::new();
     store.insert(
         "+".to_string(),
@@ -120,7 +121,7 @@ pub fn new_core_model() -> Model {
             }
             )
         );
-    Model {store}
+    Model {store, inner: None}
 }
 
 
@@ -139,9 +140,9 @@ fn parse_single_float(exp: &Oexp) -> Result<f64, RomeError> {
 }
 
 // The eval function 
-pub fn eval(exp: &Oexp, env: &mut Model) -> Result<Oexp, RomeError> {
+pub fn eval(exp: &Oexp, modl: &mut Model) -> Result<Oexp, RomeError> {
     match exp { // 1
-        Oexp::Symbol(k) => env.store.get(k)
+        Oexp::Symbol(k) => search_model(k, modl)
             .ok_or(RomeError::OperatorError(format!("Unexpected symbok k='{}'", k)))
             .map(|x| x.clone()),
         Oexp::Number(_a) => Ok(exp.clone()),
@@ -154,18 +155,19 @@ pub fn eval(exp: &Oexp, env: &mut Model) -> Result<Oexp, RomeError> {
             // and `?` as a way to check conditions in the form of 
             // `something if (condition is true) else somethingelse ?`
             let arg_forms = list.get(0..len-1).ok_or(RomeError::OperatorError("unable to get forms".to_string()))?;
-            match eval_built_in_form(last_form, arg_forms, env) { // 2
+            match eval_built_in_form(last_form, arg_forms, modl) { // 2
                 Some(res) => res,
                 None => {
                     let arg_forms = &list[0..(len - 1)];
-                    let func_eval = eval(last_form, env)?;
+                    let func_eval = eval(last_form, modl)?;
                     match func_eval { // 3
                         Oexp::Function(f) => {
-                            let args_eval = arg_forms.iter()
-                                .map(|x| eval(x, env))
-                                .collect::<Result<Vec<Oexp>, RomeError>>();
-                            f(&args_eval?)
+                            f(&eval_forms(arg_forms, modl)?)
                         },
+                        Oexp::FunctionDef(l) => {
+                            let new_modl = &mut new_model_for_fn_def(l.params_exp, arg_forms, modl)?;
+                            eval(&l.body_exp, new_modl)
+                        }
                         _ => Err(RomeError::OperatorError("The last form must be a function".to_string())),
 
                     } // match 3
@@ -177,20 +179,75 @@ pub fn eval(exp: &Oexp, env: &mut Model) -> Result<Oexp, RomeError> {
     } // match 1
 } // end eval
 
-fn eval_built_in_form(exp: &Oexp, arg_forms: &[Oexp], env: &mut Model) -> Option<Result<Oexp, RomeError>> {
+fn new_model_for_fn_def<'a>(
+    params: Rc<Oexp>,
+    arg_forms: &[Oexp],
+    inner_env: &'a mut Model,
+    ) -> Result<Model<'a>, RomeError> {
+    let ks = parse_list_of_symbol_strings(params)?;
+    if ks.len() != arg_forms.len() {
+        return Err(RomeError::ReaderError(format!("expected {} arguments, got {}", ks.len(), arg_forms.len())));
+    }
+    let vs = eval_forms(arg_forms, inner_env)?;
+    let mut store: HashMap<String, Oexp> = HashMap::new();
+    for (k, v) in ks.iter().zip(vs.iter()) {
+        store.insert(k.clone(), v.clone());
+    }
+
+    Ok(Model {
+        store,
+        inner: Some(inner_env),
+    })
+}
+
+fn parse_list_of_symbol_strings(form: Rc<Oexp>) -> Result<Vec<String>, RomeError> {
+    let list = match form.as_ref() {
+        Oexp::List(s) => Ok(s.clone()),
+        _ => Err(RomeError::ReaderError(
+                "Expected arg form to be a list".to_string()))
+    }?;
+    list.iter().map(|x| { match x {
+        Oexp::Symbol(s) => Ok(s.clone()),
+        _ => Err(RomeError::ReaderError("Expected symbols in the argument list".to_string())),
+    }}).collect()
+}
+
+fn search_model(k: &str, modl: &Model) -> Option<Oexp>{
+    match modl.store.get(k) {
+        Some(exp) => Some(exp.clone()),
+        None => {
+            match &modl.inner {
+                Some(inner_modl) => search_model(k, &inner_modl),
+                None => {
+                    // Later search the "stage" here. For now return none
+                    None
+                }
+            }
+        }
+    }
+}
+
+fn eval_forms(arg_forms: &[Oexp], modl: &mut Model) -> Result<Vec<Oexp>, RomeError> {
+    arg_forms
+        .iter()
+        .map(|x| eval(x, modl))
+        .collect()
+}
+
+fn eval_built_in_form(exp: &Oexp, arg_forms: &[Oexp], modl: &mut Model) -> Option<Result<Oexp, RomeError>> {
     match exp {
         Oexp::Symbol(s) =>
             match s.as_ref() {
-                "?" => Some(eval_query(arg_forms, env)),
-                "." => Some(eval_define(arg_forms, env)),
-                "fn" => Some(eval_function_def(arg_forms, env)),
+                "?" => Some(eval_query(arg_forms, modl)),
+                "." => Some(eval_define(arg_forms, modl)),
+                "fn" => Some(eval_function_def(arg_forms, modl)),
                 _ => None,
             },
             _ => None,
     }
 }
 
-fn eval_query(arg_forms: &[Oexp], env: &mut Model) -> Result<Oexp, RomeError> {
+fn eval_query(arg_forms: &[Oexp], modl: &mut Model) -> Result<Oexp, RomeError> {
     let len = arg_forms.len();
     let subject = arg_forms.get(0).ok_or(
         RomeError::OperatorError("expected a subject as first form in conditional".to_string()))?;
@@ -201,7 +258,7 @@ fn eval_query(arg_forms: &[Oexp], env: &mut Model) -> Result<Oexp, RomeError> {
     match verb {
         Oexp::Symbol(v) => 
             match v.as_ref() {
-                ">" => eval_gt_query(subject, object, env), 
+                ">" => eval_gt_query(subject, object, modl), 
                 "<" => unimplemented!(),
                 "=" => unimplemented!(),
                 ">=" => unimplemented!(),
@@ -210,7 +267,7 @@ fn eval_query(arg_forms: &[Oexp], env: &mut Model) -> Result<Oexp, RomeError> {
                 "if" => {
                     let rem_args = arg_forms.get(3..len).ok_or(
                         RomeError::OperatorError("expected an else/or branch to if".to_string()))?;
-                    eval_if_query(subject, object, rem_args, env)
+                    eval_if_query(subject, object, rem_args, modl)
                 },
                 _ => unimplemented!(),
             },
@@ -226,12 +283,12 @@ fn eval_gt_query(subject: &Oexp, object: &Oexp, _env: &mut Model) -> Result<Oexp
 
 }
 
-fn eval_if_query(subject: &Oexp, object: &Oexp, rem_args: &[Oexp], env: &mut Model) -> Result<Oexp, RomeError> {
-    let predicate = eval(object, env)?;
+fn eval_if_query(subject: &Oexp, object: &Oexp, rem_args: &[Oexp], modl: &mut Model) -> Result<Oexp, RomeError> {
+    let predicate = eval(object, modl)?;
     match predicate {
         Oexp::Boolean(b) => {
             match b { // 1
-                true => eval(subject, env),
+                true => eval(subject, modl),
                 false => {
                     // handle else/or branch
                     let keyword = rem_args.get(0).ok_or(
@@ -242,7 +299,7 @@ fn eval_if_query(subject: &Oexp, object: &Oexp, rem_args: &[Oexp], env: &mut Mod
                             "else" => {
                                 let something_else = rem_args.get(1).ok_or(
                                     RomeError::OperatorError("...else what?...".to_string()))?;
-                                eval(something_else, env)
+                                eval(something_else, modl)
                             },
                             "or" => {
                                 unimplemented!();
@@ -259,7 +316,7 @@ fn eval_if_query(subject: &Oexp, object: &Oexp, rem_args: &[Oexp], env: &mut Mod
     }
 }
 
-fn eval_define(arg_forms: &[Oexp], env: &mut Model) -> Result<Oexp, RomeError> {
+fn eval_define(arg_forms: &[Oexp], modl: &mut Model) -> Result<Oexp, RomeError> {
      let subject = arg_forms.get(0).ok_or(
         RomeError::OperatorError("expected a subject as first form in definition".to_string()))?;
      let name_str = match subject {
@@ -277,8 +334,8 @@ fn eval_define(arg_forms: &[Oexp], env: &mut Model) -> Result<Oexp, RomeError> {
         Oexp::Symbol(v) => 
             match v.as_ref() {
                 "=" => {
-                    let object_eval = eval(object, env)?;
-                    env.store.insert(name_str, object_eval);
+                    let object_eval = eval(object, modl)?;
+                    modl.store.insert(name_str, object_eval);
 
                     Ok(subject.clone())
                 },
